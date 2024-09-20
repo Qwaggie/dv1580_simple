6da23779db854981536fa50cacd8c3ee535f8045
Gustav Hedin <gurra.hedin@gmail.com>
2024-09-20 16:53:12 +0200

    Lade till en check för om argumentet är ett nummer

--- add.c
+++ add.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int is_number(const char *str){
+  char *endptr;
+  double value = strtod(str, &endptr);
+
+  if(endptr == str){
+    return 0;
+  }
+  return 1;
+}
+
 int main(int argc, char *argv[]){
+  if(is_number(argv[1]) == 0 || is_number(argv[2]) == 0){
+    printf("ERROR\n");
+    return 1;
+  }
   int sum = atoi(argv[1])+atoi(argv[2]);
   printf("%d\n", sum);
   return 0;
